5774a976ce909ff42bc26a7bd30d5d58c62911b2
Johnathan Barr <jbarr@smu.edu>
2019-05-27 23:20:51 -0500

    added user microposts

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
 
   def setup
-    # @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
-    @user = users(:michael)
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
+    # @user = users(:michael)
   end
 
   test "should be valid" do
@@ -70,5 +70,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
+  end
 
 end
